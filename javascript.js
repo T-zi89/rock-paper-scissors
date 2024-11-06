eaa1bb8b699e5ffa98167d26c40665d043f9cf7a
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2024-11-06 14:06:29 +0100

    start math.random function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 console.log("Hello World")
 
-function = getComputerChoice(rock,paper,scissor) {
-    math.random()
+function getComputerChoice(Rock,Paper,Scissor) {
+    if (number <= 33,33) return "Rock";
+    else if (number > 33,33, number <= 66,66) return "Paper";
+    else if (number > 66,66) return "Scissor";
 }
+
+const computerChoice = Math.random();
+let number = (computerChoice * 100)
+
+console.log(getComputerChoice())
