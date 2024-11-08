a85444190b369b7ca0613e15ac7591bab85728e0
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2024-11-08 13:41:24 +0100

    continue js computerChoice

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
 console.log("Hello World")
 
-function getComputerChoice(Rock,Paper,Scissor) {
-    if (number <= 33,33) return "Rock";
-    else if (number > 33,33, number <= 66,66) return "Paper";
-    else if (number > 66,66) return "Scissor";
-}
+//function getComputerChoice(Rock,Paper,Scissor) {
+//    if (number <= 33,33) return "Rock";
+//    else if (number > 33,33, number <= 66,66) return "Paper";
+//    else if (number > 66,66) return "Scissor";
+//}
 
 const computerChoice = Math.random();
 let number = (computerChoice * 100)
+console.log(number)
+
+function getComputerChoice(number) {
+    let choice
+    if (number < 33,33) choice = "Rock"; {
+    }    if (number > 33,33 && number < 66,66) choice = "Paper"; {
+    }   if (number > 66,66) choice = "Scissor";
+}
 
 console.log(getComputerChoice())
+console.log(choice)
