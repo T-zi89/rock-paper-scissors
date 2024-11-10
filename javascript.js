80f9c735ae38c4ff4776a91fd90a78f27bb28537
T-zi89 <piaiatiziano@gmail.com+odin@users.noreply.github.com>
2024-11-10 10:55:00 +0100

    start comparison results

--- javascript.js
+++ javascript.js
@@ -13,10 +13,39 @@ function getComputerChoice() {
 console.log(getComputerChoice())
 
 function getHumanChoice() {
-    let humanChoice = window.prompt("Choose: Rock, Paper or Scissor");
-
-
-
+    let humanChoice = window.prompt("Choose: Rock, Paper or Scissor").toLowerCase();
+    if (humanChoice === "rock") {
+        return "Rock";
+    } else if (humanChoice === "paper") {
+        return "Paper";
+    } else {
+        return "Scissor";
+    }
 }
 
 console.log(getHumanChoice())
+
+let computerScore = 0
+let humanScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    if    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "scissor") ||
+    (humanChoice === "scissor" && computerChoice === "paper") } 
+    { humanScore ++;
+        return "Win!";
+    } else if {
+    (humanChoice === computerChoice);
+    return "It's a tie, another round!";
+    }
+
+    else {
+        computerScore ++;
+        return "Dang!";
+    }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+  
